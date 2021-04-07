689562baa30497d50c1b80df9b8d28c1427bee81
Chivova <Chivovaz@ua.fm>
2021-04-07 15:49:47 +0300

    add files, update index.js and README

--- js-homework-09-food-service/src/index.js
+++ js-homework-09-food-service/src/index.js
@@ -1,1 +1,35 @@
 import './styles.css';
+import './js/theme-switch.js';
+import theme from './js/theme.js';
+
+const refs = {
+  checkbox: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
+
+refs.checkbox.addEventListener('change', onChangeThemeClick);
+
+function onChangeThemeClick(evt) {
+  const isCheckboxChecked = evt.target.checked;
+
+  if (isCheckboxChecked) {
+    refs.body.classList.add('dark-theme');
+    refs.body.classList.remove('light-theme');
+    localStorage.setItem('theme-name', theme.DARK);
+  } else {
+    refs.body.classList.add('light-theme');
+    refs.body.classList.remove('dark-theme');
+    localStorage.setItem('theme-name', theme.LIGHT);
+  }
+}
+
+currentTheme();
+
+function currentTheme() {
+  const isCurrentTheme = localStorage.getItem('theme-name') === theme.DARK;
+
+  if (isCurrentTheme) {
+    refs.body.classList.add('dark-theme');
+    refs.checkbox.checked = true;
+  }
+}
---
+++ js-homework-09-food-service/src/js/theme-switch.js
@@ -0,0 +1,2 @@
+import '../styles.css';
+import theme from '../js/theme.js';
